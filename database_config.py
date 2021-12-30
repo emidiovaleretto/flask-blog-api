85e31407286600855c8ee2002354e6d275b67cf8
Emidio Valereto <emidio.valereto@gmail.com>
2021-12-30 22:38:01 +0000

    Fix a bug with the environment secret key.

--- database_config.py
+++ database_config.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 db = SQLAlchemy(app)
